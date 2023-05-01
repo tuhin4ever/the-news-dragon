6b4ac0cb9d3f55660e6ee2c3f2133f00df822a78
tuhin_4ever <tuhin4ever19@gmail.com>
2023-05-01 14:08:20 +0600

    update rateing math rounded

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -48,7 +48,7 @@ const NewsCard = ({ news }) => {
       </Card.Body>
       <Card.Footer className="text-muted d-flex">
         <div className="flex-grow-1 d-flex align-items-center">
-          <Rating style={{ maxWidth: 150 }} value={rating} readOnly />
+          <Rating style={{ maxWidth: 150 }} value={Math.round(rating?.number || 0)} readOnly />
           <span className="ms-2">{rating?.number}</span>
         </div>
         <div>
